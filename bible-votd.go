261f8d78d95eca69ee562b9d5c2a843e926d061a
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-03-15 21:23:19 +0000

    🚸 set -h default because JSON doesn't work

--- bible-votd.go
+++ bible-votd.go
@@ -52,7 +52,7 @@ var (
 	hostname = flag.String("H", "chleb-api.daybologic.co.uk", "The hostname for the remote Chleb Bible Search server")
 	port = flag.Int("p", 0, "connect to port, default depends whether -k is specified");
 	insecureFlag = flag.Bool("k", false, "connect via legacy HTTP (insecure)");
-	htmlFlag = flag.Bool("h", false, "Use text/html")
+	htmlFlag = flag.Bool("h", true, "Use text/html")
 )
 
 func fetch(respond chan<- string, query string) {
