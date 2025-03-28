0865563d68a93da79333e7372ae1ec273757b6ba
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-03-28 21:11:17 +0000

    🐛 members JSON structs must be ^\U
    I have no idea why this in the case and it took me ages to figure out

--- remote/fetch.go
+++ remote/fetch.go
@@ -41,22 +41,22 @@ import (
 )
 
 type JSONResponseAttributes struct {
-	text string `json:"text"`
+	Text string `json:"text"`
 }
 
 type JSONResponseData struct {
-	attributes JSONResponseAttributes `json:"attributes"`
+	Attributes JSONResponseAttributes `json:"attributes"`
 }
 
 type JSONResponseLinks struct {
-	prev string `json:"prev"`
-	self string `json:"self"`
-	next string `json:"next"`
+	Prev string `json:"prev"`
+	Self string `json:"self"`
+	Next string `json:"next"`
 }
 
 type JSONResponse struct {
-	data [1]JSONResponseData `json:"data"`
-	links JSONResponseLinks `json:"links"`
+	Data [1]JSONResponseData `json:"data"`
+	Links JSONResponseLinks `json:"links"`
 }
 
 func Fetch(query string, htmlFlag bool) (response string, ok bool) {
@@ -100,9 +100,7 @@ func Fetch(query string, htmlFlag bool) (response string, ok bool) {
 		if err != nil {
 			log.Fatalf("Unable to marshal JSON due to %s", err)
 		}
-		//cookedBody = fmt.Sprintf("%s", jsonResponse.data[0].attributes.text)
-		cookedBody = fmt.Sprintf("%s", jsonResponse)
-		cookedBody = jsonResponse.links.self
+		cookedBody = fmt.Sprintf("%s", jsonResponse.Data[0].Attributes.Text)
 	}
 
 	return cookedBody, true
